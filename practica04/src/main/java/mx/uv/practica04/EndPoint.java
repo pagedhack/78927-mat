a90bf0a4569128cf5f14163a45986b45e9e1da5c
pagedhack <samu1@outlook.es>
2023-03-27 16:34:32 -0600

    Modificado

--- practica04/src/main/java/mx/uv/practica04/EndPoint.java
+++ practica04/src/main/java/mx/uv/practica04/EndPoint.java
@@ -8,6 +8,8 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import https.t4is_uv_mx.saludos.ASaludarRequest;
 import https.t4is_uv_mx.saludos.ASaludarResponse;
+import https.t4is_uv_mx.saludos.BBuscarIdRequest;
+import https.t4is_uv_mx.saludos.BBuscarResponse;
 
 @Endpoint
 public class EndPoint {
@@ -27,6 +29,27 @@ public class EndPoint {
         saludador.setNombre(peticion.getNombre());
         isaludador.save(saludador);
 
+        return response;
+    }
+
+    // @PayloadRoot(localPart = "BBuscarRequest", namespace = "https://t4is.uv.mx/saludos")
+
+    // @ResponsePayload
+    // public BBuscarResponse Pedir() {
+    //     BBuscarResponse response = new BBuscarResponse();
+    //     isaludador.findAll();
+    //     return response;
+    // }
+
+
+
+    @PayloadRoot(localPart = "BBuscarIdRequest", namespace = "https://t4is.uv.mx/saludos")
+
+    @ResponsePayload
+    public BBuscarResponse Pedir(@RequestPayload BBuscarIdRequest peticion){
+        BBuscarResponse response = new BBuscarResponse();
+        Saludador saludador = new Saludador();
+        saludador.getId();
         return response;
     }
 }
